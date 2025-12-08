f769fd8d30a901fbf07c4d11bb7ac40ebcd40a02
Yaniv89 <yanivabu8@gmail.com>
2025-12-08 11:55:49 +0200

    Build game for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ROZ/',
+  base: '/ROZ/',    
+  build: {
+    outDir: 'docs',
+  },
   server: {
     port: 3000,
     open: true
